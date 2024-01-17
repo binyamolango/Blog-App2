40e78e45fdb34d5cdd36838f6293e9db369712d8
binyamyohannes <olangobinyam@gmail.com>
2024-01-17 12:35:44 +0300

    Update the database

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,11 +3,11 @@ class Post < ApplicationRecord
   has_many :likes
   has_many :comments
 
+  after_create :update_posts_counter
+
   validates :title, presence: true, length: { maximum: 250 }
   validates :comments_count, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_count, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-
-  after_create :update_posts_counter
 
   def update_posts_counter
     author.increment!(:posts_count)
--- db/migrate/20231108070720_create_posts.rb
+++ db/migrate/20231108070720_create_posts.rb
@@ -3,8 +3,8 @@ class CreatePosts < ActiveRecord::Migration[7.1]
     create_table :posts do |t|
       t.string :title
       t.text :text
-      t.bigint :comments_count, default: 0
-      t.bigint :likes_count, default: 0
+      t.bigint :comments_count
+      t.bigint :likes_count
       t.timestamps
     end
   end
