4734b8dab6253b93e56ab60fd808f663f1fb21dd
binyamyohannes <olangobinyam@gmail.com>
2024-01-24 17:24:06 +0300

    Add test for pagination

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :set_user
 
   def index
-    @posts = @user.posts.includes(:comments)
+    @posts = @user.posts.includes(:comments).paginate(page: params[:page], per_page: 3)
   end
 
   def show
