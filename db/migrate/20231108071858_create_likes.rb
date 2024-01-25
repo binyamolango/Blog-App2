f2f645789efebc1bf5425dcc07c56a57320dd3db
binyamyohannes <olangobinyam@gmail.com>
2024-01-25 16:42:38 +0300

    Update db

--- db/migrate/20231108071858_create_likes.rb
+++ db/migrate/20231108071858_create_likes.rb
@@ -1,5 +1,12 @@
 class CreateLikes < ActiveRecord::Migration[7.1]
   def change
-    create_table :likes, &:timestamps
+    create_table :likes do |t|
+      t.timestamps
+      t.string :address
+      t.references :post, null: false, foreign_key: { on_delete: :cascade }
+      t.references :user, null: false, foreign_key: true
+    end
+
+    add_index :likes, :address
   end
 end
--- db/migrate/20231122062522_add_default_value_to_posts.rb
+++
@@ -1,6 +0,0 @@
-class AddDefaultValueToPosts < ActiveRecord::Migration[7.1]
-  def change
-    change_column :posts, :comments_count, :bigint, default: 0
-    change_column :posts, :likes_count, :bigint, default: 0
-  end
-end
