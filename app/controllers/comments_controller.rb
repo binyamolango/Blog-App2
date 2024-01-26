46b0f8cba1a3fc58268f9361ae35cc7499e1e665
binyamyohannes <olangobinyam@gmail.com>
2024-01-26 09:41:53 +0300

    Update other controllers

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,17 @@
 class CommentsController < ApplicationController
   load_and_authorize_resource
 
+  def index
+    @user = User.find(params[:user_id])
+    @post = @user.posts.find(params[:post_id])
+    @comments = @post.comments
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @comments }
+    end
+  end
+
   def new
     @comment = Comment.new
     @post = Post.find(params[:post_id])
